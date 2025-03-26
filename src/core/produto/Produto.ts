57c755eb613c4c669247f7f8b9f0441e7187dba0
gabriel <gabrielslima14k@gmail.com>
2025-03-26 20:23:49 -0300

    feat:correções

--- src/core/produto/Produto.ts
+++ src/core/produto/Produto.ts
@@ -1,7 +1,7 @@
 
-import precificavel from './precificavel';
+import {Precificavel} from './precificavel';
 import Especificacoes from './especificacoes';
-export default interface Produto extends precificavel {
+export default interface Produto extends Precificavel {
     id: number;
     nome: string;
     descricao: string;
