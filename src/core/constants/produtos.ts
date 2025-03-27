f4471e4428d31c1415e341aee84356f61cb565d6
TavinHVM <gustav0.henriqu31504@gmail.com>
2025-03-27 14:27:52 -0300

    feat: add product constants and initial product data

---
+++ src/core/constants/produtos.ts
@@ -0,0 +1,22 @@
+import Produto from "../produto/Produto";
+
+const produtos: Produto[] = [
+    {
+        id: 1,
+        nome: "Produto 1",
+        descricao: "Produto 1",
+        marca: "Marca 1",
+        modelo: "Modelo 1",
+        imagem: "https://via.placeholder.com/150",
+        videoReview: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        nota: 4.5,
+        tags: ["tag1", "tag2", "tag3"],
+        preco: 100,
+        especificacoes: {},
+        precobase: 100,
+        precopromocional: 50,
+        menorpreco: 50,
+        maiorpreco: 100,
+        precomedio: 75,
+    }
+]
