f0872276ba10fc76adb17f4c1a992ff99c9996e9
TavinHVM <gustav0.henriqu31504@gmail.com>
2025-03-29 18:52:36 -0300

    feat: add shopping cart icon component with item count display

---
+++ essencial/frontend/src/components/shared/IconeCarrinho.tsx
@@ -0,0 +1,19 @@
+import {IconShoppingCart} from "@tabler/icons-react";
+import { experimental_taintUniqueValue } from "react";
+
+export interface IconeCarrinhoProps {
+    qtdItens: number;
+}
+
+export default function IconeCarrinho(props: IconeCarrinhoProps) {
+    return (
+        <div className="relative flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full">
+            <IconShoppingCart size={30} color="#FFFF" />
+            {props.qtdItens > 0 && (
+                <div className="absolute top-0 right-0 flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">
+                    {props.qtdItens}
+                </div>
+            )}
+        </div>
+    );
+}
