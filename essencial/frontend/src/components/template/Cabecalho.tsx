5f6eed02555fd1cc35dc694231414d7456ac2b33
TavinHVM <gustav0.henriqu31504@gmail.com>
2025-03-31 16:30:38 -0300

    feat: update layout and styles for header and main components, adjust body margins and paddings

--- essencial/frontend/src/components/template/Cabecalho.tsx
+++ essencial/frontend/src/components/template/Cabecalho.tsx
@@ -5,10 +5,17 @@ import Link from "next/link";
 export default function Cabecalho() {
     return (
         <div
-            className="flex flex-col h-20"
-            style={{ background: "linear-gradient(90deg, #14002D, 100%)" }}
+            className="flex flex-col h-16 top-0 left-0 w-full z-50"
+            style={{
+                background: "linear-gradient(90deg, #14002D, 100%)",
+                margin: 0,
+            }}
         >
-            <div className="flex-1 container flex flex-col justify-center">
+            <div className="flex-1 container flex flex-col justify-center"
+            style={{
+                padding: "20px 20px 10px 20px", // Adicionado padding-bottom
+                borderBottom: "1px solid rgba(255, 255, 255, 0.5)"
+            }}>
                 <div className="flex justify-between items-center">
                     <Logo />
                     <Link href="">
--- essencial/frontend/src/components/template/Pagina.tsx
+++ essencial/frontend/src/components/template/Pagina.tsx
@@ -1,10 +1,10 @@
 import Cabecalho from "./Cabecalho";
 
-export default function Pagina(props:any) {
+export default function Pagina(props: any) {
     return (
         <div>
             <Cabecalho />
-            <main>{props.children}</main>
+            <main className="pt-16">{props.children}</main> {/* Ajustado para pt-16 */}
         </div>
-    )
+    );
 }
