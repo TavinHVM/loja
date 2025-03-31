ae16ed5ce09639d9de826b13a1e1b8f6df7832f3
gabriel <gabrielslima14k@gmail.com>
2025-03-31 20:39:38 -0300

    ✨feat: enhance IconeCarrinho component layout and improve item count display

--- essencial/frontend/src/components/shared/IconeCarrinho.tsx
+++ essencial/frontend/src/components/shared/IconeCarrinho.tsx
@@ -1,4 +1,5 @@
 import {IconShoppingCart} from "@tabler/icons-react";
+import { experimental_taintUniqueValue } from "react";
 
 export interface IconeCarrinhoProps {
     qtdItens: number;
@@ -6,11 +7,18 @@ export interface IconeCarrinhoProps {
 
 export default function IconeCarrinho(props: IconeCarrinhoProps) {
     return (
-        <div className="flex items-center justify-center w-10 h-10 bg-black-500 rounded-full relative">
-            <IconShoppingCart size={30} stroke={1.3} color="#FFFF" />
+        <div className="relative flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full">
+            <IconShoppingCart size={30} color="#FFFF" />
             {props.qtdItens > 0 && (
-                <div className="absolute top-0 right-0 flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">
-                    {props.qtdItens ?? 0}
+                <div className="
+                absolute top-0 
+                right-0 flex 
+                items-center 
+                justify-center
+                w-5 h-5 text-xs
+                font-bold text-white 
+                bg-red-500 rounded-full">
+                    {props.qtdItens}
                 </div>
             )}
         </div>
