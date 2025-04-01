971a524e972f227b56f5de4c313e96b2b108878e
TavinHVM <gustav0.henriqu31504@gmail.com>
2025-04-01 00:11:19 -0300

    feat: update Pagina component to include a full-screen background image and enhance layout structure

--- essencial/frontend/src/components/template/Pagina.tsx
+++ essencial/frontend/src/components/template/Pagina.tsx
@@ -3,10 +3,21 @@ import Rodape from "./Rodape";
 
 export default function Pagina(props: any) {
     return (
-        <div>
-            <Cabecalho />
-            <main className="pt-16">{props.children}</main>
-            <Rodape />
+        <div className="flex flex-col min-h-screen">
+            <div
+                className="flex-1 flex flex-col w-screen"
+                style={{
+                    backgroundImage: "url('/background.png')",
+                    backgroundSize: "cover", // Faz a imagem cobrir toda a tela
+                    backgroundRepeat: "no-repeat", // Evita repetição da imagem
+                    backgroundPosition: "center", // Centraliza a imagem
+                    backgroundAttachment: "fixed", // Fixa o background ao rolar a página
+                }}
+            >
+                <Cabecalho />
+                <main className="pt-16">{props.children}</main>
+                <Rodape />
+            </div>
         </div>
     );
 }
