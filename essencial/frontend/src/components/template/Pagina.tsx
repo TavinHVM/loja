06db7809c55be438700e737f93e664afed568bef
gabriel <gabrielslima14k@gmail.com>
2025-04-02 14:36:57 -0300

    feat: enhance Pagina component with background image support and improved layout structure

--- essencial/frontend/src/components/template/Pagina.tsx
+++ essencial/frontend/src/components/template/Pagina.tsx
@@ -2,6 +2,11 @@ import Cabecalho from "./Cabecalho";
 import Rodape from "./Rodape";
 
 
+
+export interface PaginaProps {
+ className?: string;
+ children:any
+}
 export default function Pagina(props: any) {
     return (
         <div
@@ -10,10 +15,21 @@ export default function Pagina(props: any) {
                 background: 'radial-gradient(50% 50% at 50% 0%, #000000, #141414)',
             }}
         >
+            <div
+                className="flex-1 flex-col w-screen"
+                style={{background:'url("/background.png") no-repeat center center fixed', backgroundSize: 'cover'}}
+                
+            >
 
             <Cabecalho/>
-            <main className="flex-1">{props.children}</main>
+            <main className={`
+            flex-1 flex-col py-10 ${props.className}''}
+            `}>{props.children}</main>
             <Rodape />
+            </div>
+            
+    
+            
         </div>
     );
 }
