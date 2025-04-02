0ad4b9427f482f9560224e7bad5cf1340a984b5a
gabriel <gabrielslima14k@gmail.com>
2025-04-02 14:10:16 -0300

    feat: update Pagina component to use a radial gradient background and simplify layout structure

--- essencial/frontend/src/components/template/Pagina.tsx
+++ essencial/frontend/src/components/template/Pagina.tsx
@@ -1,27 +1,19 @@
 import Cabecalho from "./Cabecalho";
 import Rodape from "./Rodape";
 
+
 export default function Pagina(props: any) {
     return (
-        <div className="flex flex-col min-h-screen">
-            <div
-                className="flex-1 flex flex-col w-screen"
-                style={{
-                    backgroundImage: "url('/background.png')",
-                    backgroundSize: "cover", // Faz a imagem cobrir toda a tela
-                    backgroundRepeat: "no-repeat", // Evita repetição da imagem
-                    backgroundPosition: "center", // Centraliza a imagem
-                    backgroundAttachment: "fixed", // Fixa o background ao rolar a página
-                }}
-            >
-                <div>
-                    
-                </div>
-                
-                <Cabecalho />
-                <main className="pt-16">{props.children}</main>
-                <Rodape />
-            </div>
+        <div
+            className="flex flex-col min-h-screen"
+            style={{
+                background: 'radial-gradient(50% 50% at 50% 0%, #000000, #141414)',
+            }}
+        >
+
+            <Cabecalho/>
+            <main className="flex-1">{props.children}</main>
+            <Rodape />
         </div>
     );
 }
