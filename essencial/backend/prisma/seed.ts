948b77e63ea6344cb39e02292e0eaf269c803a17
TavinHVM <gustav0.henriqu31504@gmail.com>
2025-04-03 01:46:24 -0300

    feat: update TypeScript version and improve seed script error handling

--- essencial/backend/prisma/seed.ts
+++ essencial/backend/prisma/seed.ts
@@ -4,12 +4,19 @@ import { produtos } from '../src/core';
 const prisma = new PrismaClient();
 
 async function seed() {
-  await prisma.produto.createMany({
-    data: produtos.map((produto) => ({
-      ...produto,
-      id: undefined,
-    })),
-  });
+  try {
+    console.log('Inserindo produtos...');
+
+    await prisma.produto.createMany({
+      data: produtos,
+    });
+
+    console.log('Seed executado com sucesso!');
+  } catch (error) {
+    console.error('Erro ao rodar o seed:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 seed();
