fa0c280c2b82c539c54cdf40f697d5e836719707
TavinHVM <gustav0.henriqu31504@gmail.com>
2025-04-03 12:32:39 -0300

    feat: adicionar ProdutoPrisma como provider no módulo Produto

--- essencial/backend/src/produto/produto.module.ts
+++ essencial/backend/src/produto/produto.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProdutoController } from './produto.controller';
+import { ProdutoPrisma } from './produto.prisma';
 
 @Module({
     
     controllers: [ProdutoController],
+    
+    providers: [ProdutoPrisma],
     
   })
   export class ProdutoModule {}({})
